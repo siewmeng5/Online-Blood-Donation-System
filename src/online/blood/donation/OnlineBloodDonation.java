c13fdc06d0c9093752d9a063bb8f4bf171aae3ea
Boon <Boon@Boon>
2022-02-04 16:37:00 +0800

    changes dffv

--- src/online/blood/donation/OnlineBloodDonation.java
+++ src/online/blood/donation/OnlineBloodDonation.java
@@ -15,7 +15,7 @@ public class OnlineBloodDonation {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-            System.out.println("helo");
+        System.out.println("helo billy");
     }
 
     public OnlineBloodDonation() {
