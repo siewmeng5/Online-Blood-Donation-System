f2e6332fcbdae7330006d954df2175f20aa84bc2
Boon <Boon@Boon>
2022-02-05 08:37:07 +0800

    changes new

--- src/online/blood/donation/OnlineBloodDonation.java
+++ src/online/blood/donation/OnlineBloodDonation.java
@@ -15,8 +15,8 @@ public class OnlineBloodDonation {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        System.out.println("testing");
+
+        System.out.println("testing 123");
 
     }
 
